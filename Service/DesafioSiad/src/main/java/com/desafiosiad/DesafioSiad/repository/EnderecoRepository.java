0953893587c11f6c189ad45e611439e20a468fa9
João Vitor Moreira <94926952+Moreira22@users.noreply.github.com>
2024-06-26 15:08:36 -0300

    feat: create EnderecoRepository.java

---
+++ Service/DesafioSiad/src/main/java/com/desafiosiad/DesafioSiad/repository/EnderecoRepository.java
@@ -0,0 +1,11 @@
+package com.desafiosiad.DesafioSiad.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.desafiosiad.DesafioSiad.model.Endereco;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, String> {
+    
+}
