34973cf251b90e8007a37c5c88f961d9ab46e627
João Vitor Moreira <94926952+Moreira22@users.noreply.github.com>
2024-06-26 14:57:44 -0300

    feat: create Venda.java

---
+++ Service/DesafioSiad/src/main/java/com/desafiosiad/DesafioSiad/model/Venda.java
@@ -0,0 +1,42 @@
+package com.desafiosiad.DesafioSiad.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@Entity
+@EqualsAndHashCode(of = "id")
+public class Venda {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column
+    private Number total;
+
+    @Column
+    private Number quantidade;
+
+    @ManyToOne
+    @JoinColumn(name = "fisica_id")
+    private Fisica fisica;
+
+    @ManyToOne
+    @JoinColumn(name = "produto_id")
+    private Produto produto;
+
+    public Venda(Number total, Number quantidade,Fisica fisica, Produto produto){
+        this.fisica = fisica;
+        this.produto = produto;
+        this.quantidade = quantidade;
+        this.total = total;
+    }
+
+}
