5e159093decc18f0d4991febb9fca220f08c5745
João Vitor Moreira <94926952+Moreira22@users.noreply.github.com>
2024-06-26 14:57:17 -0300

    feat: create Endereco.java

---
+++ Service/DesafioSiad/src/main/java/com/desafiosiad/DesafioSiad/model/Endereco.java
@@ -0,0 +1,45 @@
+package com.desafiosiad.DesafioSiad.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@Entity
+@EqualsAndHashCode(of = "id")
+public class Endereco {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column
+    private String cep;
+
+    @Column
+    private String cidade;
+
+    @Column
+    private String bairro;
+
+    @Column
+    private String rua;
+
+    @Column
+    private Number numero;
+
+    @OneToOne(mappedBy = "endereco")
+    private Pessoa pessoa;
+
+    public Endereco(String cep, String cidade, String bairro, String rua, Number numero) {
+        this.cep = cep;
+        this.cidade = cidade;
+        this.bairro = bairro;
+        this.rua = rua;
+        this.numero = numero;
+    }
+}
