4e5ce37320682d3d4ba8224787cfd74e15bff5fa
João Vitor Moreira <94926952+Moreira22@users.noreply.github.com>
2024-07-03 18:49:28 -0300

    feat: create form.tsx

---
+++ Web/desafiosiad/src/app/produto/components/form.tsx
@@ -0,0 +1,119 @@
+"use client"
+import * as React from "react";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import * as o from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { toast } from "@/components/ui/use-toast";
+import { Juridico } from "@/model/juridico";
+import { usejuridico } from "@/hooks/empresaAuth";
+import { Input } from "@/components/ui/input";
+
+interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+export function FormProduto({ className, ...props }: UserAuthFormProps) {
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+  const { PostJuridico } = usejuridico();
+
+  const formSchema = o.object({
+    nome: o.string().min(3, {
+      message: "Campo `Nome` precisa ter mais que 3 caracteres.",
+    }),
+    valor: o.string().min(3, {
+      message: "Campo `CNPJ` precisa ter mais que 3 digitos.",
+    }),
+  });
+
+  const form = useForm<o.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      nome: "",
+      valor: "",
+    },
+  });
+
+  async function onSubmit(values: o.infer<typeof formSchema>) {
+    const value = {
+      nome: values.nome,
+      valor: values.valor,
+    };
+    setIsLoading(true);
+
+    try {
+      await PostJuridico(value);
+    } catch (error) {
+      console.error("Erro ao fazer Cadastro de Vededor", error);
+      toast({
+        title: "Erro inesperado!",
+        description: "Não foi cadastra Vededor",
+        variant: "destructive",
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  return (
+    <div className={cn("w-[500px] grid gap-6", className)} {...props}>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-1">
+          <FormField
+            control={form.control}
+            name="nome"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-[#00000]">Nome</FormLabel>
+                <FormControl>
+                  <Input
+                    className="border border-[#A7A7A7]"
+                    placeholder="Nome"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="valor"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-[#00000]">Valor:</FormLabel>
+                <FormControl>
+                  <Input
+                    className="border border-[#A7A7A7]"
+                    placeholder="Valor"
+                    {...field}
+                    type="number"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="flex flex-row justify-end p-6 gap-3">
+            <Button
+              className="  bg-primary flex float-end border  border-[#A7A7A7] h-10 w-50% rounded-full px-6 py-2 text-sm ring-offset-background file:border-0 fi
+                        le:bg-transparent file:text-sm file:font-medium  focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 
+                        disabled:cursor-not-allowed disabled:opacity-50 gap-3"
+              type="submit"
+            >
+              Cadastrar
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </div>
+  );
+}
